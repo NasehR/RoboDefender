e6f7a7a73e6a7b14e5b64156993b397d10429036
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-08-25 15:42:38 +0800

    Changed the name of the application. displays button 1 and 2, logger displays if which button is pressed.

--- src/main/java/edu/curtin/saed/assignment1/App.java
+++ src/main/java/edu/curtin/saed/assignment1/App.java
@@ -6,47 +6,53 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class App extends Application 
+public class App extends Application
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        launch();        
+        launch();
     }
-    
+
     @Override
-    public void start(Stage stage) 
+    public void start(Stage stage)
     {
-        stage.setTitle("Example App (JavaFX)");
+        stage.setTitle("RoboDefender");
         JFXArena arena = new JFXArena();
-        arena.addListener((x, y) ->
-        {
-            System.out.println("Arena click at (" + x + "," + y + ")");
-        });
-        
+        TextArea logger = new TextArea();
         ToolBar toolbar = new ToolBar();
         Button btn1 = new Button("My Button 1");
         Button btn2 = new Button("My Button 2");
         Label label = new Label("Score: 999");
-//        toolbar.getItems().addAll(btn1, btn2, label);
-        toolbar.getItems().addAll(label);
 
-         btn1.setOnAction((event) ->
-         {
-             System.out.println("Button 1 pressed");
-         });
-                    
-        TextArea logger = new TextArea();
-        logger.appendText("Hello\n");
-        logger.appendText("World\n");
-        
+        arena.addListener((x, y) ->
+                System.out.println("Arena click at (" + x + "," + y + ")")
+        );
+
+        toolbar.getItems().addAll(btn1, btn2, label);
+
+        btn1.setOnAction((event) ->
+        {
+            System.out.println("Button 1 pressed");
+            logger.appendText("Button 1 pressed\n");
+        });
+
+
+        btn2.setOnAction((event) ->
+        {
+            System.out.println("Button 2 pressed");
+            logger.appendText("Button 2 pressed\n");
+        });
+
+        logger.appendText("Welcome to RoboDefender\n");
+
         SplitPane splitPane = new SplitPane();
         splitPane.getItems().addAll(arena, logger);
         arena.setMinWidth(300.0);
-        
+
         BorderPane contentPane = new BorderPane();
         contentPane.setTop(toolbar);
         contentPane.setCenter(splitPane);
-        
+
         Scene scene = new Scene(contentPane, 800, 800);
         stage.setScene(scene);
         stage.show();
