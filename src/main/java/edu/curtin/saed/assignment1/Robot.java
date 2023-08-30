be8e86d6d19294e6b568dae9e6673d108a5f8884
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-08-30 16:00:43 +0800

    Rearranged the methods to have the setters then the getters. Modiefied so that it does not take in a image as a parameter instead loads it when initialising the object.

--- src/main/java/edu/curtin/saed/assignment1/Robot.java
+++ src/main/java/edu/curtin/saed/assignment1/Robot.java
@@ -1,26 +1,77 @@
 package edu.curtin.saed.assignment1;
 
-import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
+import javafx.scene.image.Image;
 
 public class Robot
 {
-    private int id;
-    private int delay;
-    private double startX;
-    private double startY;
+    private static final String IMAGE_FILE = "rg1024-robot-carrying-things-4.png";
+
+    private String id;
+    private int d;
+    private double xPos;
+    private double yPos;
     public RobotState robotState;
     private Image image;
 
-
-    public Robot (int id)
+    public Robot (String id)
     {
+        try(InputStream is = getClass().getClassLoader().getResourceAsStream(IMAGE_FILE))
+        {
+            if(is == null)
+            {
+                throw new AssertionError("Cannot find image file " + IMAGE_FILE);
+            }
+            image = new Image(is);
+        }
+        catch(IOException e)
+        {
+            throw new AssertionError("Cannot load image file " + IMAGE_FILE, e);
+        }
         this.id = id;
-        delay = delay();
+        this.d = delay();
         robotState = new Alive(this);
     }
 
-    public int delay()
+    public void setPosition(double xPos, double yPos)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public void setState(RobotState newState)
+    {
+        this.robotState = newState;
+    }
+
+    public String getName()
+    {
+        return this.id;
+    }
+
+    public Image getImage()
+    {
+        return this.image;
+    }
+
+    public double getXPos()
+    {
+        return this.xPos;
+    }
+
+    public double getYPos()
+    {
+        return this.yPos;
+    }
+
+    public int getDelay()
+    {
+        return this.d;
+    }
+
+    public static int delay()
     {
         Random random = new Random();
 
@@ -33,24 +84,9 @@ public class Robot
         return random.nextInt(range) + minRange;
     }
 
-    public void setPosition(double startX, double startY)
-    {
-        this.startX = startX;
-        this.startY = startY;
-    }
-
-    public void setState(RobotState newState)
-    {
-        this.robotState = newState;
-    }
-
     public void dead()
     {
         robotState.dead();
     }
 
-    public void addImage(Image image)
-    {
-        this.image = image;
-    }
 }
