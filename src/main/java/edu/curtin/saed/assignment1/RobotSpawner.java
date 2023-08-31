d875f6c53a6bd308f7d4bb501e4b308ddc216387
Naseh <75029758+NasehR@users.noreply.github.com>
2023-08-31 22:47:33 +0800

    Class responsible for randomly generating robots on the arean.

---
+++ src/main/java/edu/curtin/saed/assignment1/RobotSpawner.java
@@ -0,0 +1,56 @@
+package edu.curtin.saed.assignment1;
+
+import java.util.Random;
+
+public class RobotSpawner implements Runnable {
+
+    private final int TOP_LEFT = 0;
+    private final int TOP_RIGHT = 1;
+    private final int BOTTOM_LEFT = 2;
+    private final int BOTTOM_RIGHT = 3;
+    private JFXArena arena;
+    private int robotCount;
+
+    public RobotSpawner(JFXArena arena) {
+        this.arena = arena;
+        robotCount = 0;
+    }
+    @Override
+    public void run() {
+        Random random = new Random();
+
+        int corner = random.nextInt(4); // Randomly choose a corner (0 to 3)
+        double xPos, yPos;
+
+        System.out.println(corner);
+
+        switch (corner) {
+            case TOP_LEFT: // Top-left corner
+                xPos = 0.0;
+                yPos = 0.0;
+                break;
+            case TOP_RIGHT: // Top-right corner
+                xPos = 8.0;
+                yPos = 0.0;
+                break;
+            case BOTTOM_LEFT: // Bottom-left corner
+                xPos = 0.0;
+                yPos = 8.0;
+                break;
+            case BOTTOM_RIGHT: // Bottom-right corner
+                xPos = 8.0;
+                yPos = 8.0;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + corner);
+        }
+
+        Robot newRobot = new Robot(Integer.toString(++robotCount));
+        newRobot.setPosition(xPos, yPos);
+        try {
+            arena.addRobot(newRobot);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
