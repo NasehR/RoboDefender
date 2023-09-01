93aca86b1fc158f6ce14bba480ac8b02624f4b3a
Naseh <75029758+NasehR@users.noreply.github.com>
2023-09-01 08:11:17 +0800

    Seprated the runnable into a method that returns a runnable.

--- src/main/java/edu/curtin/saed/assignment1/RobotManager.java
+++ src/main/java/edu/curtin/saed/assignment1/RobotManager.java
@@ -1,5 +1,6 @@
 package edu.curtin.saed.assignment1;
 
+import java.util.Objects;
 import java.util.concurrent.*;
 
 public class RobotManager implements Runnable {
@@ -8,8 +9,10 @@ public class RobotManager implements Runnable {
     private JFXArena arena;
     private ExecutorService robotThreadPool;
     private BlockingQueue<RobotSpawner> robots;
+    private Object mutex;
 
     public RobotManager (JFXArena arena) {
+        mutex = new Object();
         this.arena = arena;
         robots = new ArrayBlockingQueue<>(10);
         robotThreadPool = Executors.newFixedThreadPool(NUM_THREADS);
@@ -18,14 +21,24 @@ public class RobotManager implements Runnable {
     @Override
     public void run() {
         // Task to generate new robots
-        Runnable robotSpawnTask = () -> {
-             RobotSpawner robot = new RobotSpawner(arena);
-             robot.spawnRobots();
-            System.out.println("Hello");
-        };
+        Runnable robotSpawnTask = spawnRobots();
 
         Thread robotSpawnThread = new Thread(robotSpawnTask);
         robotThreadPool.submit(robotSpawnThread);
+    }
+
+    private Runnable spawnRobots () {
+        Runnable robotSpawnTask = () -> {
+            try {
+                RobotSpawner robot = new RobotSpawner(arena);
+                robot.spawnRobots();
+                System.out.println("Hello");
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        return robotSpawnTask;
     }
 
     public void shutdown() {
