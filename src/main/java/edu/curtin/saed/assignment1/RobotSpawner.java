1a3bb0bf1bb7def5e1bdfcb5a437a7b4e7b42b18
Naseh <75029758+NasehR@users.noreply.github.com>
2023-09-01 08:10:12 +0800

    Robots generating every 1500ms. Need to check if the square is empty before displaying.

--- src/main/java/edu/curtin/saed/assignment1/RobotSpawner.java
+++ src/main/java/edu/curtin/saed/assignment1/RobotSpawner.java
@@ -16,43 +16,47 @@ public class RobotSpawner {
         robotCount = 0;
     }
 
-    public void spawnRobots() {
-        Random random = new Random();
+    public void spawnRobots() throws InterruptedException {
+        for (int i = 0; i < 3; i++)
+        {
+            Thread.sleep(1500);
+            Random random = new Random();
 
-        int corner = random.nextInt(4); // Randomly choose a corner (0 to 3)
-        double xPos, yPos;
+            int corner = random.nextInt(4); // Randomly choose a corner (0 to 3)
+            double xPos, yPos;
 
-        System.out.println(corner);
+    //        System.out.println(corner);
 
-        switch (corner) {
-            case TOP_LEFT: // Top-left corner
-                xPos = 0.0;
-                yPos = 0.0;
-                break;
-            case TOP_RIGHT: // Top-right corner
-                xPos = 8.0;
-                yPos = 0.0;
-                break;
-            case BOTTOM_LEFT: // Bottom-left corner
-                xPos = 0.0;
-                yPos = 8.0;
-                break;
-            case BOTTOM_RIGHT: // Bottom-right corner
-                xPos = 8.0;
-                yPos = 8.0;
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + corner);
-        }
+            switch (corner) {
+                case TOP_LEFT: // Top-left corner
+                    xPos = 0.0;
+                    yPos = 0.0;
+                    break;
+                case TOP_RIGHT: // Top-right corner
+                    xPos = 8.0;
+                    yPos = 0.0;
+                    break;
+                case BOTTOM_LEFT: // Bottom-left corner
+                    xPos = 0.0;
+                    yPos = 8.0;
+                    break;
+                case BOTTOM_RIGHT: // Bottom-right corner
+                    xPos = 8.0;
+                    yPos = 8.0;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + corner);
+            }
 
-        Robot newRobot = new Robot(Integer.toString(++robotCount));
-        newRobot.setPosition(xPos, yPos);
+            Robot newRobot = new Robot(Integer.toString(++robotCount));
+            newRobot.setPosition(xPos, yPos);
 
-        try {
-            arena.addRobot(newRobot);
-            moveRobot(newRobot);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            try {
+                arena.addRobot(newRobot);
+                moveRobot(newRobot);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
