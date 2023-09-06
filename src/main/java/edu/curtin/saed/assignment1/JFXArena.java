e4d99d98e6c98510aae745885ecf0dbd6a824294
Naseh <75029758+NasehR@users.noreply.github.com>
2023-09-06 23:49:55 +0800

    Added an addWall method to the class.

--- src/main/java/edu/curtin/saed/assignment1/JFXArena.java
+++ src/main/java/edu/curtin/saed/assignment1/JFXArena.java
@@ -3,6 +3,7 @@ package edu.curtin.saed.assignment1;
 import javafx.scene.canvas.*;
 import javafx.geometry.VPos;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
@@ -161,7 +162,6 @@ public class JFXArena extends Pane
 
     public void addWall(Wall wall) {
         this.wall = wall;
-        layoutChildren();
     }
 
     private void drawWall(GraphicsContext gfx, Wall wall) {
