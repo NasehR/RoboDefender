268b12811f3038788a9738bae3736130455e58bd
Naseh <75029758+NasehR@users.noreply.github.com>
2023-09-09 02:42:33 +0800

    Programmed the movement of the robot to meet the design requirements.

--- src/main/java/edu/curtin/saed/assignment1/RobotSpawner.java
+++ src/main/java/edu/curtin/saed/assignment1/RobotSpawner.java
@@ -1,5 +1,7 @@
 package edu.curtin.saed.assignment1;
 
+import javafx.application.Platform;
+
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import static java.lang.Math.*;
@@ -85,54 +87,51 @@ public class RobotSpawner {
     private void moveRobot(Robot robot) throws InterruptedException {
         Runnable moveRobotTask = () -> {
             try {
-                while (robot.getXPos() != 4.0 && robot.getYPos() != 4.0) {
-                    Thread.sleep(robot.getDelay());
-                    int xDiff, yDiff;
-                    int newX, newY;
-                    double moveStep = arena.getGridSquareSize()/400;
-                    /*
-                    get the available squares to move into.
-                    randomly choose a square move into that square.
-                     */
-                    xDiff = (8 - (int) robot.getXPos());
-                    yDiff = (8 - (int) robot.getYPos());
+                double moveStep = arena.getGridSquareSize() / 500; // Divide the movement into 40 steps
+                int targetX = 4; // Target X coordinate (citadel's X coordinate)
+                int targetY = 4; // Target Y coordinate (citadel's Y coordinate)
 
-                    /*
-                    if (xDiff < yDiff) {
-                        // loop while the new pos isn't reached
-                        newX = (int) robot.getXPos();
-                        newY = (int) robot.getYPos() + yDiff;
-                        for (int i = 0; i < 400; i++) {
-                            synchronized (lock) {
-                                if ((!arena.isCoordinateOccupied(newX, newY)) || arena.isCoordinateOccupiedByWall(newX, newY)){
-                                    robot.setYPosition(robot.getYPos() + moveStep);
-                                }
-                                System.out.println(robot.getName() + ": MOVE y WARDS");
+                while (robot.getXPos() != targetX && robot.getYPos() != targetY) {
+                    Thread.sleep(robot.getDelay()); // Divide the delay into 40 steps
+
+                    // Calculate the difference between the current position and the target
+                    int xDiff = targetX - (int) robot.getXPos();
+                    int yDiff = targetY - (int) robot.getYPos();
+
+                    // Determine the direction to move (horizontal or vertical)
+                    if (Math.abs(xDiff) >= Math.abs(yDiff)) {
+                        // Move horizontally
+                        int newX = (int) robot.getXPos() + (xDiff > 0 ? 1 : -1);
+                        int newY = (int) robot.getYPos();
+                        synchronized (lock) {
+                            if (isValidMove(newX, newY)) {
+                                robot.setXPosition(robot.getXPos() + (xDiff > 0 ? moveStep : -moveStep));
                             }
                         }
-                         Thread.sleep(40);
-                    }
-                    else if (xDiff >= yDiff){
-                        newX = (int) robot.getXPos() + xDiff;
-                        newY = (int) robot.getYPos();
-                        // loop while the new pos isn't reached
-                        for (int i = 0; i < 400; i++) {
-                            synchronized (lock) {
-                                if ((!arena.isCoordinateOccupied(newX, newY)) || arena.isCoordinateOccupiedByWall(newX, newY)) {
-                                    robot.setXPosition(robot.getXPos() + moveStep);
-                                }
-                                System.out.println(robot.getName() + ": MOVE x WARDS");
+                    } else {
+                        // Move vertically
+                        int newX = (int) robot.getXPos();
+                        int newY = (int) robot.getYPos() + (yDiff > 0 ? 1 : -1);
+                        synchronized (lock) {
+                            if (isValidMove(newX, newY)) {
+                                robot.setYPosition(robot.getYPos() + (yDiff > 0 ? moveStep : -moveStep));
                             }
-                            Thread.sleep(40);
                         }
                     }
 
-                     */
+                    // Redraw the arena to show the updated robot position
+                    Platform.runLater(() -> arena.layoutChildren());
                 }
-
-            } catch (InterruptedException e) { }
+            }
+            catch (InterruptedException e) { }
         };
 
         threadPool.submit(moveRobotTask);
     }
+
+    private boolean isValidMove(int newX, int newY) {
+        return (!arena.isCoordinateOccupied(newX, newY) || arena.isCoordinateOccupiedByWall(newX, newY))
+                && newX >= 0 && newX < 9 && newY >= 0 && newY < 9;
+    }
+
 }
