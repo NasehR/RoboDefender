00d4409fbfa86492cda94b2850177f46a452e0d9
Naseh <75029758+NasehR@users.noreply.github.com>
2023-09-12 22:37:21 +0800

    updated code.

--- src/main/java/edu/curtin/saed/assignment1/WallBuilder.java
+++ src/main/java/edu/curtin/saed/assignment1/WallBuilder.java
@@ -2,7 +2,7 @@ package edu.curtin.saed.assignment1;
 
 import javafx.application.Platform;
 import javafx.scene.control.TextArea;
-
+import javafx.scene.control.Label;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -17,22 +17,29 @@ public class WallBuilder {
     private final Object mutex;
     private ExecutorService threadPool;
     private TextArea logger;
+    private Label wallLabel;
 
-    public WallBuilder(JFXArena arena, ExecutorService threadPool, TextArea logger) {
+    public WallBuilder(JFXArena arena, ExecutorService threadPool, TextArea logger,  Label wallLabel) {
         this.arena = arena;
         buildQueue = new ArrayBlockingQueue<>(MAX_WALLS);
         mutex = new Object();
         this.threadPool = threadPool;
         this.logger = logger;
+        this.wallLabel = wallLabel;
     }
 
     public void run () {
         Runnable addWallTask = () -> {
             try {
                 while (true) {
+//                    int wallsBuilt = arena.numberOfWalls();
                     synchronized (mutex) {
                         Wall wall = buildQueue.take();
+                        int wallsToBeBuilt = buildQueue.size();
                         Platform.runLater(() -> {
+
+//                            int wallsLeftToBuild = arena.numberOfWalls() - buildQueue.size();
+                            wallLabel.setText("\t\t\tWalls in the queue: " + wallsToBeBuilt);
                             arena.addWall(wall);
                             int xPosition = (int) wall.getXPos() + 1;
                             int yPosition = (int) wall.getYPos() + 1;
